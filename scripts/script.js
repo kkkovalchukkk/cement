58dd0fefd5cc8e5cdb2e02f684c9d0ec1e92df1a
kkkovalchukkk <kkkova1chukkk@yandex.ru>
2023-06-25 00:59:57 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,32 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
+const swiper_second = new Swiper(".advantages-section__swiper", {
+  direction: "horizontal",
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".advantages-section__swiper-right-btn",
+    prevEl: ".advantages-section__swiper-left-btn",
+  },
+});
+
+const swiper = new Swiper(".promo-section__swiper", {
   direction: "horizontal",
   slidesPerView: 1,
   loop: true,
 
-  // Navigation arrows
   navigation: {
     nextEl: ".promo-section__swiper-navigation-right",
     prevEl: ".promo-section__swiper-navigation-left",
+  },
+});
+
+const swiper_third = new Swiper(".goods-section__swiper", {
+  direction: "horizontal",
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
